bb8d6686877f0c2b2ace77b77ebd3fa47fa0b0ea
Neil Scheuermann <neilscheuermann@gmail.com>
2018-11-12 21:08:17 -0700

    re-did workshop up to Start Seeding, but then got stuck

--- seed.js
+++ seed.js
@@ -1,38 +1,14 @@
-const { db, Gardener, Plot, Vegetable } = require('./models');
+const db = require('./models');
 
 db.sync({force: true})
   .then(() => {
-    console.log("Success!");
+    console.log('Database synced!');
+  })
+  .catch(error => {
+    console.log('Disaster! Something went wrong!');
+    console.log(error);
+  })
+  .finally(() => {
+    console.log('Closing database.')
     db.close();
   })
-  .catch(err => {
-    console.log('Error...:', err);
-    db.close();
-  });
-
-Vegetable.create({
-  name: "carrot",
-  color: "orange",
-  plantedOn: new Date()
-});
-
-const PlotVegetable = db.model('plot_vegetable');
-
-return PlotVegetable.create({
-  vegetableId: carrot.id,
-  plotId: plot1.id
-});
-
-// db.sync({force: true})
-// .then(() => {
-//   console.log('Database synced!')
-//   // db.close() // only if using a version of node without `finally`
-// })
-// .catch(err => {
-//   console.log('Disaster! Something went wrong! ')
-//   console.log(err)
-//   // db.close() // only if using a version of node without `finally`
-// })
-// .finally(() => { // only if using a version of node WITH `finally`
-//   db.close()
-// })
